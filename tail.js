00044ce98a13351c7f86972bbee5099bba06bce5
Eiman <imansawi>
2021-01-12 18:03:38 -0500

    refactored

--- tail.js
+++ tail.js
@@ -1,22 +1,12 @@
-const assertNonEmptyArr = function(inputArray) {
-  if (inputArray.length > 0) {
-    console.log(`✅✅✅ Assertion Passed!`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inputArray} Array Has No Elements!`);
-  }
-  return;
-};
-  
 // FUNCTION RETURNS TAIL OF AN ARRAY
 const tail = function(myArray) {
-  assertNonEmptyArr(myArray);
-  let firstElement = myArray.shift();
-  console.log(myArray);
+  if (myArray.lengt > 0) {
+    let firstElement = myArray.shift();
+    console.log(myArray);
+  }
 };
-  
 
-tail([1, 2, 3]);
-tail([]);
-tail(["Hello!"]);
-tail([5,6,7]);
-tail(["Hello", "Lighthouse", "Labs"]);
+module.exports = tail;
+
+
+
