3dc771a7a6d8f1cb5aa72bf357eb87c526fba305
Eiman <imansawi>
2021-01-12 17:53:22 -0500

    refactored

--- assertEqual.js
+++ assertEqual.js
@@ -1,19 +1,17 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  //let mesg = "qqqqq";
   if (actual !== expected) {
-  //  mesg = "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected;
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   } else {
-  //  mesg = "✅✅✅ Assertion Passed: " + actual + " === " + expected;
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
-  //console.log(mesg);
   return;
 };
 
+module.exports = assertEqual;
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, -2);
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, -2);
