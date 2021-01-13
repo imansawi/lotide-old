936b5c2eb153862ac7763e38197123aa1b8c29b2
Eiman <imansawi>
2021-01-12 19:19:01 -0500

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,33 +1,20 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2, ) {
-    let isEqual = true;  
-    if ((array1.length !== 0) && (array2.length !== 0) && (array1.length === array2.length)) {
-      for (i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          isEqual = false;
-          break;
-        }
+// FUNCTION TESTS IF TWO ARRAYS ARE EQUAL
+
+const eqArrays = function(array1, array2) {
+  if (
+    array1.length !== 0 &&
+    array2.length !== 0 &&
+    array1.length === array2.length
+  ) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
       }
-    } else {
-      isEqual = false;
     }
-    return isEqual;
-  };    
-
-
-const assertEqual = function(passed) {
-    if (passed === true) {
-    console.log("Assertion Passed!");
   } else {
-    console.log("Assertion Failed!");
+    return false;
   }
+  return true;
 };
   
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
