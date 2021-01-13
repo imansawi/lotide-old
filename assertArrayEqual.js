5e4b94060379a1d964c6c9413cd693a6e925c242
Eiman <imansawi>
2021-01-12 19:35:47 -0500

    duplicated file removed

--- assertArrayEqual.js
+++
@@ -1,9 +0,0 @@
-const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
-
-// FUNCTION IMPLEMENTATION
-const assertArrayEqual = function(array1, array2) {
-  assertEqual(eqArrays(array1, array2));
-};
-
-module.exports = assertArrayEqual;
