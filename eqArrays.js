ce90b161af16c804fb5bba7df6c30a5a26fe0bc5
Eiman <imansawi>
2021-01-12 23:22:23 -0500

    modified

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,22 @@
 // FUNCTION TESTS IF TWO ARRAYS ARE EQUAL
 
 const eqArrays = function(array1, array2) {
-  if (
+  if (array1 === undefined || array2 === undefined) {
+    if (array1 === undefined && array2 === undefined) {
+      return true;
+    }
+    return false;
+  }
+  if 
+  (
     array1.length !== 0 &&
     array2.length !== 0 &&
     array1.length === array2.length
   ) {
+    let arr1 = array1.sort();
+    let arr2 = array2.sort();
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
@@ -16,5 +25,6 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-  
+
 module.exports = eqArrays;
+
